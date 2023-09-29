b993ccd2f7ee7838cb1cd03736f0e9e004212da2
Ritwik <ritwik.upadhyay888@gmail.com>
2023-09-29 13:40:01 +0530

    Initial commit to setup API project

---
+++ src/main/java/com/blogbreeze/app/api/BlogBreezeAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.blogbreeze.app.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBreezeAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBreezeAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogbreeze/app/api/BlogBreezeAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogbreeze.app.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBreezeAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
